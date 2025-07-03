73d59a32f9648352a2f6f3665fefbd8988c715a7
matifape <matiasalmasia1@gmail.com>
2025-07-03 18:33:37 -0300

    avances

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { gsap } from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
     <>
-    <div className="flex-center h-[100vh]">
-        <h1 className="text-5xl text-indigo-800">Mojito</h1>
-    </div>
+    <Navbar />
+    <Hero />
+   
       
     </>
   );
