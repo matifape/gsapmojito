421e6a3d04d53e2307f1ce056b7c0b3df943eb4c
matifape <matiasalmasia1@gmail.com>
2025-07-03 11:32:36 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { gsap } from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <>
+    <div className="flex-center h-[100vh]">
+        <h1 className="text-5xl text-indigo-800">Mojito</h1>
+    </div>
+      
+    </>
+  );
+};
+
+export default App;
